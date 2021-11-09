569efb7253881c705696d34e2b093bb8132ec9d6
Fatih Baser <basese_aehit@students.vizja.pl>
2021-11-09 11:44:50 +0100

    İmplemented navigation

---
+++ app/src/main/java/com/fatihbaserpl/artbookhilt/view/ArtDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.fatihbaserpl.artbookhilt.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.fatihbaserpl.artbookhilt.R
+
+class ArtDetailsFragment: Fragment(R.layout.fragment_art_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/fatihbaserpl/artbookhilt/view/MainActivity.kt
+++ app/src/main/java/com/fatihbaserpl/artbookhilt/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fatihbaserpl.artbookhilt
+package com.fatihbaserpl.artbookhilt.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fatihbaserpl.artbookhilt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
